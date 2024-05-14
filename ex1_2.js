842368aef18b6bbeca289125818eeefaeade3577
onumam21 <onumam56@gmail.com>
2024-05-14 14:08:25 +0700

    Ex1'2

---
+++ ex1_2.js
@@ -0,0 +1,7 @@
+function inform(firstName, location, hobby) {
+    console.log("Hi, my name is " + firstName + ". I live in " + location + " and enjoy " + hobby + ".");
+  }
+
+  inform("John", "New York", "reading");
+  
+  inform("Jane", "Los Angeles", "hiking");
